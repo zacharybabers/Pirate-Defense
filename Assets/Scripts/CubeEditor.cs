6e5b3b74aed3814f08709c79e834f5a9aeaaaa72
WizfanZZ <zacharybabers@gmail.com>
2020-09-12 21:32:33 -0700

    Got Cubes running with coordinate text meshes

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+
+    [SerializeField] [Range(0f, 100f)] private float gridSize = 10;
+
+    private TextMesh textMesh;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * gridSize;
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
